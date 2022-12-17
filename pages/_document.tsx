3f9c9b8a4b0476709d5de25eeecbf569054f62e5
Jenis Jesuratnam <jenis.jesuratnam@hotmail.com>
2022-12-17 15:22:34 -0500

    chore: add tinymce

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/4/tinymce.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
