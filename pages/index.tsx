6a9b7e0eaa9520ea8e568b8fe6ece81c39618e0d
Jenis Jesuratnam <jenis.jesuratnam@hotmail.com>
2022-12-17 15:24:44 -0500

    chore: remove script

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,25 @@
-import { Inter } from "@next/font/google";
-import { Editor } from "@tinymce/tinymce-react";
-
-const inter = Inter({ subsets: ["latin"] });
+import {Editor} from "@tinymce/tinymce-react";
 
 export default function Home() {
   const onEditorChange = (e: { [p: string]: any }) => {
     console.log(e?.target?.getContent());
-  };
+  }
 
-  console.log(process.env.TINY_URL);
+  console.log(process.env.TINY_URL)
 
   return (
     <>
-      <title>Script Editor</title>
+    <title>Script Editor</title>
       <h1>Script Editor</h1>
       <Editor
-        initialValue="<p>This is the initial content of the editor</p>"
-        init={{
-          plugins: "link image code",
-          toolbar:
-            "undo redo | bold italic | alignleft aligncenter alignright | code",
-        }}
-        onChange={(e) => onEditorChange(e)}
+          apiKey={process.env.TINY_URL}
+          initialValue="<p>This is the initial content of the editor</p>"
+          init={{
+            plugins: 'link image code',
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+          }}
+          onChange={(e) => onEditorChange(e)}
       />
-    </>
-  );
+      </>
+  )
 }
